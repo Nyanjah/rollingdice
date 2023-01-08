f8072542d7c8ede2bd11b86c1d8c09892af46446
Nyanjah <jason.zingaretti@gmail.com>
2023-01-07 23:02:11 -0500

    added vertex rasterization- discovered rotate bug

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,138 @@
 mod physics;
 mod rasterize;
+use minifb::*;
+use physics::*;
+use std::f64::consts::PI;
 
+const WIDTH: usize = 100;
+const HEIGHT: usize = 100;
+const SECONDS_PER_FRAME: f32 = 0.2;
 
-fn main(){
+struct Camera {
+    transform: Transform,
+    buffer: Vec<Vec<u8>>,
+    width: usize,
+    height: usize,
+}
+
+impl Transformable for Camera {
+    fn transform(&self) -> &Transform {
+        &self.transform
+    }
+    fn transform_mut(&mut self) -> &mut Transform {
+        &mut self.transform
+    }
+}
+//NOTE: Assuming camera is NOT rotated for current implementation...
+impl Camera {
+    fn new(position: &[f64; 3], width: &usize, height: &usize) -> Self {
+        return Camera {
+            transform: Transform {
+                position: *position,
+                quaternion: Quaternion::new(0.0, &[1.0, 1.0, 1.0]),
+            },
+            buffer: vec![vec![0;*height];*width],
+            width: *width,
+            height: *height,
+        };
+    }
+
+    fn update_buffer(&mut self, world: &Vec<Cube>) {
+        // Getting the (x,y) position of the top-left most point of the camera
+        let (x_0, y_0) = (
+            self.transform.position[0] - 0.5 * self.width as f64,
+            self.transform.position[1] + 0.5 * self.height as f64,
+        );
+        for cube in &*world{
+        let vertices = cube.get_vertices();
+
+        // For each (x,y) pixel in the camera's grid:
+        for y in 0..self.height {
+            for x in 0..self.width {
+                // Calculate where the current sample point lies in relation to the world
+                let (x_i, y_i) = (x_0 + x as f64 + 0.5, y_0 - y as f64 - 0.5);
+                for &vertex in &vertices {
+                    // If the vertex is within 1-pixel radius of the sample grid point:
+                    if ((vertex[0] - x_i).powi(2) + (vertex[1] - y_i).powi(2)).sqrt() <= 1.0 {
+                        // Add the grayscale pixel value to the buffer
+                        self.buffer[x][y] = 255 as u8;
+                        break
+                    }
+                    else {
+                        //self.buffer[x][y] = 0 as u8;
+                    }
+                    
+                }
+            }
+        }
+    }
+    }
     
+    pub fn buffer(&self) -> &Vec<Vec<u8>>{
+        return &self.buffer;
+    }
+    pub fn clear_buffer(&mut self){
+        self.buffer = vec![vec![0;self.height];self.width];
+        
+    }
+}
 
 
+fn main() {
+    // Setting up the window
+    let mut window = Window::new(
+        "Rasterization",
+        WIDTH,
+        HEIGHT,
+        WindowOptions {
+            borderless: false,
+            title: true,
+            resize: true,
+            scale: Scale::X4,
+            scale_mode: ScaleMode::Stretch,
+            transparency: false,
+            none: false,
+            topmost: false,
+        },
+    )
+
+
+
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+    let initial_rotation = Quaternion::new(0.0, &[1.0, 1.0, 1.0]);
+    let cube1 = Cube::new(35.0, &[0.0, 0.0, 0.0], initial_rotation);
+    let cube2 = Cube::new(35.0/2.0, &[40.0, -50.0, 0.0], initial_rotation);
+    let cube3 = Cube::new(35.0/3.0, &[-40.0, 0.0, 0.0], initial_rotation);
+    let mut camera = Camera::new(&[0.0, 0.0, 10.0], &WIDTH, &HEIGHT);
+    let rotation1 = Quaternion::new(PI / 10.0, &[1.0, 1.0, 1.0]);
+    let rotation2 = Quaternion::new(PI / 20.0, &[-1.0, -1.0, 1.0]);
+    let mut world = Vec::new();
+    world.push(cube1);
+    world.push(cube2);
+    world.push(cube3);
+   
+    let mut window_buffer: Vec<u32> = Vec::new();
+    window.limit_update_rate(Some(std::time::Duration::from_secs_f32(SECONDS_PER_FRAME)));
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for cube in &mut world {
+            cube.rotate(&rotation1);
+            cube.rotate(&rotation2);
+        }
+        camera.update_buffer(&world);
+        
+        for x in 0..WIDTH{
+            for y in 0..HEIGHT{
+                let pixel = camera.buffer()[x][y] as u32;
+                window_buffer.push(
+                    (pixel | pixel << 8 | pixel << 16) as u32
+                )
+            }
+        }
+
+        window.update_with_buffer(&window_buffer, WIDTH, HEIGHT).unwrap();
+        camera.clear_buffer();
+        window_buffer.clear();
+    }
 }
--- src/physics.rs
+++ src/physics.rs
@@ -1,57 +1,134 @@
+#![allow(dead_code)]
+
 use std::iter::zip;
-#[derive(Copy,Clone)]
-struct Quaternion{
-    angle:f32,
-    x:f32,
-    y:f32,
-    z:f32
+#[derive(Copy, Clone)]
+pub struct Quaternion {
+    pub angle: f64,
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+pub struct Cube {
+    side_length: f64,
+    transform: Transform,
 }
 
-impl std::ops::Mul for Quaternion{
+pub struct Transform {
+    pub position: [f64; 3],     // Position vector
+    pub quaternion: Quaternion, // Rotation quaternion to track orientation in world
+}
+
+pub trait Transformable {
+    fn transform(&self) -> &Transform;
+    fn transform_mut(&mut self) -> &mut Transform;
+
+    fn translate(&mut self, vector: &[f64; 3]) {
+        for (mut pos, translation) in zip(self.transform_mut().position, vector) {
+            pos += translation;
+        }
+    }
+
+    fn rotate(&mut self, quaternion: &Quaternion) {
+        self.transform_mut().quaternion = self.transform_mut().quaternion * *quaternion;
+    }
+}
+
+impl std::ops::Mul for Quaternion {
     type Output = Quaternion;
     fn mul(self, rhs: Self) -> Self::Output {
         let product = Quaternion {
-            angle: self.angle * rhs.angle - (self.x*rhs.x + self.y*rhs.y + self.z*rhs.z),
+            angle: self.angle * rhs.angle - (self.x * rhs.x + self.y * rhs.y + self.z * rhs.z),
             x: self.angle * rhs.x + self.x * rhs.angle + self.y * rhs.z - self.z * rhs.y,
-            y: self.angle * rhs.y - self.x * rhs.z + self.y * rhs.angle + self.z*rhs.x,
-            z: self.angle * rhs.z + self.x * rhs.y - self.y * rhs.x + self.z * rhs.angle
+            y: self.angle * rhs.y - self.x * rhs.z + self.y * rhs.angle + self.z * rhs.x,
+            z: self.angle * rhs.z + self.x * rhs.y - self.y * rhs.x + self.z * rhs.angle,
         };
         return product;
     }
 }
 
-// impl std::ops::Add for Quaternion{
-//     type Output = Quaternion;
-//     fn add(self, rhs: Self) -> Self::Output {
-        
-//     }
-// }
+impl Quaternion {
+    pub fn new(angle: f64, axis: &[f64; 3]) -> Self {
+        let magnitude = (axis[0].powf(2.0) + axis[1].powf(2.0) + axis[2].powf(2.0)).sqrt();
+        Quaternion {
+            angle: (angle / 2.0).cos(),
+            x: axis[0] * (angle / 2.0).sin() / magnitude,
+            y: axis[1] * (angle / 2.0).sin() / magnitude,
+            z: axis[2] * (angle / 2.0).sin() / magnitude,
+        }
+    }
 
-struct Cube {
-    side_length: f32,   
-    position: [f32;3],     // Position vector tracks center of cube
-    quaternion: Quaternion // Rotation quaternion to track orientation in world
+    pub fn get_inverse(&self) -> Quaternion {
+        Quaternion {
+            angle: self.angle,
+            x: -1.0 * self.x,
+            y: -1.0 * self.y,
+            z: -1.0 * self.z,
+        }
+    }
+}
+
+impl From<&[f64; 3]> for Quaternion {
+    fn from(vector: &[f64; 3]) -> Self {
+        return Quaternion {
+            angle: 0.0,
+            x: vector[0],
+            y: vector[1],
+            z: vector[2],
+        };
+    }
+}
+
+impl Transformable for Cube {
+    fn transform(&self) -> &Transform {
+        &self.transform
+    }
+    fn transform_mut(&mut self) -> &mut Transform {
+        &mut self.transform
+    }
 }
 
 impl Cube {
-    pub fn new(side_length: f32, position: [f32;3], quaternion:Quaternion) -> Self {
+    pub fn new(side_length: f64, position: &[f64; 3], quaternion: Quaternion) -> Self {
         if side_length <= 0.0 {
             panic!("Attempted to instantiate cube of side length <= 0.");
         }
         Cube {
-            side_length:side_length,
-            position:(position),
-            quaternion: quaternion,
+            side_length: side_length,
+            transform: Transform {
+                position: *position,
+                quaternion: quaternion,
+            },
         }
     }
 
-    pub fn translate(&mut self, vector:&[f32;3]){
-        for (mut pos,translation) in zip(self.position,vector){
-            pos += translation;
+    pub fn get_vertices(&self) -> Vec<[f64; 3]> {
+        let mut vertices: Vec<[f64; 3]> = Vec::new();
+        let length = self.side_length / 2.0;
+        let (x_0, y_0, z_0) = (
+            self.transform.position[0],
+            self.transform.position[1],
+            self.transform.position[2],
+        );
+        // Initial state -> Translation -> Rotation -> Resulting Vertices
+        for x in [1.0, -1.0] {
+            for y in [-1.0, 1.0] {
+                for z in [-1.0, 1.0] {
+                    // Initial vertices after the translation step
+                    vertices.push([length * x + x_0, length * y + y_0, length * z + z_0]);
+                }
+            }
         }
-    }
+        // Applying the cube's stored rotation quaternion
+        let vertices = vertices
+            .iter()
+            .map(|vertex| {
+                //p -> q * p * q^-1
+                let rotation_results = (self.transform.quaternion * Quaternion::from(vertex))
+                    * self.transform.quaternion.get_inverse();
+                [rotation_results.x, rotation_results.y, rotation_results.z]
+            })
+            .collect();
 
-    pub fn rotate(&mut self,quaterion: Quaternion){          
-        self.quaternion = self.quaternion * quaterion;
+        return vertices;
     }
 }
